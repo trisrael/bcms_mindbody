21813177d03ff3a2399892efa0692a95f498cac4
Tristan Goffman <tgoffman@gmail.com>
2011-06-02 21:19:44 -0700

    Update routes with versioning turned off... and namespaced, and update generator to point to newly named migration

--- lib/bcms_mindbody/routes.rb
+++ lib/bcms_mindbody/routes.rb
@@ -2,5 +2,10 @@ module Cms::Routes
   def routes_for_bcms_mindbody
     match "/staff/by_collection/:name", :to => "staff#using_portlet", :method => :get, :defaults => {:format=> 'json'}
     match "/staff.json", :to => "staff#index", :method => :get, :defaults => {:format=> 'json'}
+	namespace :cms do
+			#Add extra 's' due to usage of string#classify method
+		    content_blocks :mindbody_optionss
+		    content_blocks :staff_optionss
+	end
   end
 end
--- lib/generators/bcms_mindbody/install/install_generator.rb
+++ lib/generators/bcms_mindbody/install/install_generator.rb
@@ -3,7 +3,7 @@ require 'cms/module_installation'
 class BcmsMindbody::InstallGenerator < Cms::ModuleInstallation
   add_migrations_directory_to_source_root __FILE__
   
-  copy_migration_file '20110529021707_mindbody_options.rb'
-  
+  copy_migration_file '20110529021707_create_mindbody_options.rb'
+  copy_migration_file '20110602161007_add_join_options_to_portlet.rb'  
 end
 
