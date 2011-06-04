4a7999f0e89f1dcd46e32497d6ba5b928daafdde
Tristan Goffman <tgoffman@gmail.com>
2011-06-04 11:05:11 -0700

    remove unnecessary inflections for mindbody options

--- lib/bcms_mindbody/engine.rb
+++ lib/bcms_mindbody/engine.rb
@@ -3,14 +3,5 @@ require 'browsercms'
 module BcmsMindbody
   class Engine < Rails::Engine
     include Cms::Module
-
-   initializer :bcms_mindbody_inflections, :after =>	:load_config_initializers do 
-		ActiveSupport::Inflector.inflections do |inflect|
-		   inflect.uncountable 'MindbodyOptions'
-		   inflect.uncountable 'mindbody_options'	
-		   inflect.uncountable 'staff_options'
-		   inflect.uncountable 'StaffOptions'
-		end
-	end
   end
 end
