2440719631959ffdf06d1b2fb693c395a00f115a
Tristan Goffman <tgoffman@gmail.com>
2011-06-19 13:47:28 -0700

    Switch from Cms::ContentType to just ContentType

--- db/migrate/20110615064736_add_subclasses_of_parameter_partials.rb
+++ db/migrate/20110615064736_add_subclasses_of_parameter_partials.rb
@@ -7,13 +7,14 @@ class AddSubclassesOfParameterPartials < ActiveRecord::Migration
   end
 
   def self.up
-  	Cms::ContentType.create!(:name => Consts::Site, :group_name => Consts::Group)
-	Cms::ContentType.create!(:name => Consts::Staff, :group_name => Consts::Group)	    	    
+  	ContentType.create!(:name => Consts::Site, :group_name => Consts::Group)
+	ContentType.create!(:name => Consts::Staff, :group_name => Consts::Group)	    	    
   end
 
   def self.down
-	Cms::ContentType.delete_all(['name = ?', Consts::Site])
+	ContentType.delete_all(['name = ?', Consts::Site])
 	CategoryType.delete_all(['name = ?', Consts::Site])
+
 	Cms::ContentType.delete_all(['name = ?', Consts::Staff])
 	CategoryType.delete_all(['name = ?', Consts::Staff])
   end
