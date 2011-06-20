4ddf1597fb436edb3c5fc9cd0082129e5b512499
Tristan Goffman <tgoffman@gmail.com>
2011-06-19 19:30:06 -0700

    Add labels of radio buttons and checkboxes

--- app/portlets/helpers/staff_portlet_helper.rb
+++ app/portlets/helpers/staff_portlet_helper.rb
@@ -2,17 +2,13 @@
 # All methods from this helper will be available in the render.html.erb for PhotoGalleryPortlet
 module StaffPortletHelper
 
-  def split_list_into_pairs(list)
-    rows = []
-    list.each_with_index do |item, i|
-      if i % 2 == 0
-        rows << [item]
-      else
-        rows.last << item
-      end
-    end
-
-    return rows
-  end
+	#Simple helpers for giving HTML id's for multiple radio buttons and checkboxes
+	def checkbox_id(id)
+		"checkbox_" + id
+	end
+	
+	def radio_id(id)
+		"radio_" + id
+	ebd
   
 end
