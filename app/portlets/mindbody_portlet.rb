d726f45eb2a38467871d0650c0f7f4714dd189c3
Tristan Goffman <tgoffman@gmail.com>
2011-06-19 22:35:06 -0700

    add source parameter_partial_id git status

--- app/portlets/mindbody_portlet.rb
+++
@@ -1,5 +0,0 @@
-class MindbodyPortlet < Portlet
-	has_many :parameter_partial_associations
-	has_many :parameter_partials, :through => :parameter_partial_associations
-	accepts_nested_attributes_for :parameter_partials
-end
