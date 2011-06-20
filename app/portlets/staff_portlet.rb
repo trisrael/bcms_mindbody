c11b1bd795cac6e90bcdc49a55fdcc24d056815c
Tristan Goffman <tgoffman@gmail.com>
2011-06-19 21:08:12 -0700

    attempting to get automatic addition of staff_parameter_partials through use array form id

--- app/portlets/staff_portlet.rb
+++ app/portlets/staff_portlet.rb
@@ -1,7 +1,7 @@
 class StaffPortlet < MindbodyPortlet
   has_many :staff_parameter_partials
   has_one :site_parameter_partial
-  validates_presence_of :site_parameter_partial
+  validates_presence_of :site_parameter_partial_id, :message => "need to supply Source Credentials to authenticate with Mindbody Service"
 
   # Mark this as 'true' to allow the portlet's template to be editable via the CMS admin UI.
   enable_template_editor true
