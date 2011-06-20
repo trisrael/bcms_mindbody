5e5cbf8951cbca2b8ec8cd960d044e0ffc18a048
Tristan Goffman <tgoffman@gmail.com>
2011-06-19 19:35:52 -0700

    hack since inclusion of portlet helpers for modules seems to be working incorrectly

--- app/portlets/helpers/staff_portlet_helper.rb
+++ app/portlets/helpers/staff_portlet_helper.rb
@@ -2,13 +2,6 @@
 # All methods from this helper will be available in the render.html.erb for PhotoGalleryPortlet
 module StaffPortletHelper
 
-	#Simple helpers for giving HTML id's for multiple radio buttons and checkboxes
-	def checkbox_id(id)
-		"checkbox_" + id
-	end
-	
-	def radio_id(id)
-		"radio_" + id
-	ebd
+
   
 end
