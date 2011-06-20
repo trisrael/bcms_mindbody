bda4b536a06cac08ebbf5826122872cf7500e6a8
Tristan Goffman <tgoffman@gmail.com>
2011-06-19 22:53:11 -0700

    parameter_partial in :source options instead of parameter_partial_id

--- app/portlets/staff_portlet.rb
+++ app/portlets/staff_portlet.rb
@@ -1,7 +1,7 @@
 class StaffPortlet < Portlet
   has_many :parameter_partial_associations, :foreign_key => "mindbody_portlet_id"
-  has_one :site_parameter_partial, :through => :parameter_partial_associations, :foreign_key => "mindbody_portlet_id", :source => "parameter_partial_id"
-  has_many :staff_parameter_partials, :through => :parameter_partial_associations, :foreign_key => "mindbody_portlet_id", :source => "parameter_partial_id"
+  has_one :site_parameter_partial, :through => :parameter_partial_associations, :foreign_key => "mindbody_portlet_id", :source => :parameter_partial
+  has_many :staff_parameter_partials, :through => :parameter_partial_associations, :foreign_key => "mindbody_portlet_id", :source => :parameter_partial
 
   validates_presence_of :site_parameter_partial_id, :message => "need to supply Source Credentials to authenticate with Mindbody Service"
 
