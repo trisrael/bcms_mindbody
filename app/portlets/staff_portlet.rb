a2fa83545f95a5868e5986532418657b98882515
Tristan Goffman <tgoffman@gmail.com>
2011-06-20 22:25:18 -0700

    add validation to portlet

--- app/portlets/staff_portlet.rb
+++ app/portlets/staff_portlet.rb
@@ -4,7 +4,7 @@ class StaffPortlet < Portlet
   has_many :site_parameter_partials, :through => :site_parameter_partial_associations, :foreign_key => "mindbody_portlet_id", :source => :parameter_partial, :limit => 1, :before_add => :delete_others
   has_many :staff_parameter_partials, :through => :staff_parameter_partial_associations, :foreign_key => "mindbody_portlet_id", :source => :parameter_partial
 
-  validates_presence_of :site_parameter_partial, :message => "need to supply Source Credentials to authenticate with Mindbody Service"
+  before_save :ensure_more_than_one
 	
   # Mark this as 'true' to allow the portlet's template to be editable via the CMS admin UI.
   enable_template_editor true
@@ -26,6 +26,13 @@ class StaffPortlet < Portlet
   def delete_others
 	#A bit wasteful but gets the job done
     self.site_parameter_partials.delete_all
+  end
+
+  def ensure_more_than_one
+    arr = site_parameter_partials
+    unless arr && arr.first != nil && arr.size < 1
+		errors.add :source_credentials, "are needed in order to talk to Mindbody service"
+	end
   end
     
 end
