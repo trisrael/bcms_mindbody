e00b9afbc3870d7bbc57aa5bb506be6a03b2fda5
Tristan Goffman <tgoffman@gmail.com>
2011-06-21 08:24:35 -0700

    simplify staff routing

--- lib/bcms_mindbody/routes.rb
+++ lib/bcms_mindbody/routes.rb
@@ -1,7 +1,8 @@
 module Cms::Routes
   def routes_for_bcms_mindbody
     match "/staff/by_collection/:name", :to => "staff#using_portlet", :method => :get, :defaults => {:format=> 'json'}
-    match "/staff.json", :to => "staff#index", :method => :get, :defaults => {:format=> 'json'}
+    match "/staff", :to => "staff#index", :method => :get, :defaults => {:format=> 'json'}
+
 	namespace :cms do		
 		    content_blocks :parameter_partials
 		    content_blocks :site_parameter_partials
